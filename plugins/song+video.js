112b0a62017fabd81c32b38662c2a24cfa4d4425
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-17 16:28:43 +0200

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -36,7 +36,7 @@ let downloadurl = down.dl_url
 
 //send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY AUSTIN MAX"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY AUSTIN MAX 💖"},{quoted:mek})
 
 }catch(e){
 console.log(e)
@@ -80,7 +80,7 @@ let downloadurl = down.dl_url
 
 //send video + document message
 await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"MADE BY AUSTIN MAX"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"MADE BY AUSTIN MAX 💖"},{quoted:mek})
 
 }catch(e){
 console.log(e)
