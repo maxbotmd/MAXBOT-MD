8878518a32d8e65c77411bb940e49996b8ab0af1
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-18 12:07:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "GNIH1IYK#7RGy7hzA0ojo1BKR0wk_kX1uiXHCzGIAutMPzYleNjM",
-ALIVE_IMG: process.eve.ALIVE_IMG || "https://telegra.ph/file/d0fdb53f9f22eefd486cb.jpg",
-ALIVE_MSG: process.eve.ALIVE_MSG || "Hello, I am MAXBOT-MD, I am alive now!",
-};
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d0fdb53f9f22eefd486cb.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am MAXBOT-MD, I am alive now!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+}; 
