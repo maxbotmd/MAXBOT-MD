28e4d04385b404d7027b9c913402e1fbf05ffd7e
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-19 19:48:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `MAXBOT-MD connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/d0fdb53f9f22eefd486cb.jpg` }, caption: up })
 
 }
 })
