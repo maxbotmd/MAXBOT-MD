4136a5fa62ce133ef794591c754479463d4aa750
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-20 18:51:58 +0200

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,10 +11,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:* ${runtime(process.uptime())} 
-*Ram usage:* ${(process.memoryUsage().heapUsed /1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* Austin Max
+let status = `
+*MAXBOT-MD SYSTEM STATUS*
+*🕑Uptime:* ${runtime(process.uptime())} 
+*🧮Ram usage:* ${(process.memoryUsage().heapUsed /1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🖥HostName:* ${os.hostname()}
+*👤Owner:* Austin Max
 `
 return reply(`${status}`) 
   
