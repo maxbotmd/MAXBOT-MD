1afc8f518ad78c193fd5770ab83ec114558565da
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-20 20:00:01 +0200

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,7 +12,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `
-*💻XBOT-MD SYSTEM STATUS💻*
+*⚙️MAXBOT-MD SYSTEM STATUS⚙️*
 
 *🕑Uptime:* ${runtime(process.uptime())} 
 *🧮Ram usage:* ${(process.memoryUsage().heapUsed /1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
