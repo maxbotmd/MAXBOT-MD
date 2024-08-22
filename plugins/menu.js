2b2e0b01c6e090def0bbd123fcc938b337312e4e
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-22 10:11:52 +0200

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,64 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: '',
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+  }
+}
+
+let madeMenu = `👋 *Hello ${pushname}*
+> *DOWNLOAD COMMANDS* ⬇️
+
+${menu.download}
+
+> *MAIN COMMANDS* 🔰
+
+${menu.main}
+
+> *GROUP COMMANDS* 🚻
+
+${menu.groups}
+
+> *OWNER COMMANDS* 💝
+
+${menu.owner}
+
+> *CONVERT COMMANDS* ♻️
+
+${menu.convert}
+
+> *SEARCH COMMANDS* 🔍
+
+${menu.search}
+
+POWERED BY AUSTIN MAX 💝
+`
+await conn.sendMessage(from,{text:madeMenu},{quoted:mek})
+
+
+
+  
+
+  }catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
