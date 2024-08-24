db5343d393573b6a6aa794a54fcd71322fd331c2
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-24 16:09:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
 
 const ownerNumber = ['265997133051']
 
@@ -43,6 +42,10 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //=================================================
+const {readEnv} = require ('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//============================================
         
 console.log("Connecting MAXBOT-MD 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
