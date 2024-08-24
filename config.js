5760b530b321539f35f0c1c0d0ef41364603f1e8
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-24 15:49:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "iVMxALKb#Jx_yARt6VJBGTiOQRSwjKjfPTTrsTdgt5m-1RgsAujo",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d0fdb53f9f22eefd486cb.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am MAXBOT-MD, I am alive now!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true to turn ON // false to turn OFF
-MODE: process.env.MODE || "public", // inbox // groups// inbox and groups
+MONGODB: process.env.MONGODB || "",
+
 }; 
