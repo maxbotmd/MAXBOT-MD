017c94d475bc249bb4b54298f0cfcc326671520b
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-27 08:04:21 +0200

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -1,5 +1,6 @@
+const { fetchJson } = require('../lib/functions')
 const config = require('../config')
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command')
 
 
 cmd({
