07553d2d9e8e7379acca71305bce0970c1b1694e
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-27 09:50:10 +0200

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -11,8 +11,8 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!q && !q.startsWith("https://")) return reply("give me fb url")
-    let data = await getJson(`https://raganork.tk/api/lyrics?query=${encodeURIComponent(match[1])}`)
+if (!q && !q.startsWith("https://")) return reply("give me name of artist and title")
+    let data = await getJson(`https://api.maher-zubair.xyz/search/lyrics?apikey=232ce576cc78a1dfab&q=pain${encodeURIComponent(match[1])}`)
     if (!result.status) return await message.sendReply('_No results found!_')
     return await message.sendReply(`${ch}Title: "${result.title}"\n\n${result.result}${ch}`);
   
