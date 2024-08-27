ae32694fc6d7dd973a2a808a230e06eb9d6488d9
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-27 09:08:29 +0200

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -11,10 +11,9 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!match[1]) return await message.sendReply("_Need query!_");
-    let result = await getJson(`https://raganork.tk/api/lyrics?query=${encodeURIComponent(match[1])}`)
+if (!q && !q.startsWith("https://")) return reply("give me fb url")
+    let data = await getJson(`https://raganork.tk/api/lyrics?query=${encodeURIComponent(match[1])}`)
     if (!result.status) return await message.sendReply('_No results found!_')
-    let ch = '```'
     return await message.sendReply(`${ch}Title: "${result.title}"\n\n${result.result}${ch}`);
   
 }catch(e){
