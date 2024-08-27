a1c2a00e7e54cc51669238b82bac30ca3c518a4f
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-08-27 10:09:07 +0200

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -2,6 +2,11 @@ const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
 
+let baseUrl;
+(async () => {
+    let baseUrlGet = await fetchJson(`https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json`)
+    baseUrl = baseUrlGet.api
+})();
 
 cmd({
     pattern: "lyric",
@@ -12,7 +17,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 if (!q && !q.startsWith("https://")) return reply("give me name of artist and title")
-    let data = await getJson(`https://api.maher-zubair.xyz/search/lyrics?apikey=232ce576cc78a1dfab&q=pain${encodeURIComponent(match[1])}`)
+    let data = await fetchJson(`${baseUrl}/api/lyrics?url=${q}`)
     if (!result.status) return await message.sendReply('_No results found!_')
     return await message.sendReply(`${ch}Title: "${result.title}"\n\n${result.result}${ch}`);
   
