d53ec222c97788866666200267f09445b51bed6c
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-09-07 16:10:23 +0200

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -35,10 +35,10 @@ await conn.sendMessage(from,{image:{url:data.thumbnail},caption:desc},{qouted:me
 //download audio 
 
 let down = await fg.yta(url)
-let downloadUrl = down.dl_url
+let downloadUrl = down.dl_url (⬆️)
 
 //send audio + document message
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*MADE BY AUSTIN MAX 💖*"},{quoted:mek})
+(⬇️) await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*MADE BY AUSTIN MAX 💖*"},{quoted:mek})
     
 }catch(e){
 console.log(e)
