9515680f82f7c35775100d744d3eaa0e08999896
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-09-08 10:11:04 +0200

    MAXBOT-MD v1.0

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ fetchLatestBaileysVersion,
 Browsers
 } = require('@whiskeysockets/baileys')
 
+
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
 const P = require('pino')
