9f7ce6818ca766230be0fbaae194fd243e93a462
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-09-24 06:16:13 +0200

    MAXBOT-MD v1.0

--- plugins/alive.js
+++ plugins/alive.js
@@ -5,7 +5,6 @@ cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
-    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
@@ -16,4 +15,6 @@ console.log(e)
 reply(`${e}`)
 }
 })
+
+
 
