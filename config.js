1795cfb1749e7386e84222d859d74dceaca070af
Austin Max <153365145+maxbotmd@users.noreply.github.com>
2024-11-20 06:55:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "V70FhLpQ#-Hy9Aqvm1JsJm83mV5aAXK3tC3MBnUsNzT08zU6qWgc",
+SESSION_ID: process.env.SESSION_ID || "fMABRaDA#sjjKkvP-oR7PFOBDKRKuQ-QljBpUdcWIAr20qMLBGe0",
 MONGODB: process.env.MONGODB || "mongodb+srv://maxaustin37:Chiputula.123@cluster0.rk3qd.mongodb.net/",
 ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "true",
 }; 
